81029523e6287e7f36f21c02c3a93d739a4206b9
dar0j <dinx_05@hotmail.com>
2023-06-02 23:58:12 -0400

    better t2 part 2

--- src/main/scala/gwent/field/Field.scala
+++ src/main/scala/gwent/field/Field.scala
@@ -15,8 +15,26 @@ class Field(var _weatherZone = new ArrayBuffer[Weather](1),
             var _pcSiegeZone = new fieldZone[SiegeUnit],
             var _pcRangeZone = new fieldZone[RangeUnit],
             var _pcCloseCombatZone = new fieldZone[CloseCombatUnit]) {
+  def weatherZone_(n) = {
+    _weatherZone = n
+  }
   def userSiegeZone_(n) = {
     _userSiegeZone = n
+  }
+  def userRangeZone_(n) = {
+    _userRangeZone = n
+  }
+  def userCloseCombatZone_(n) = {
+    _userCloseCombatZone = n
+  }
+  def pcSiegeZone_(n) = {
+    _pcSiegeZone = n
+  }
+  def pcRangeZone_(n) = {
+    _pcRangeZone = n
+  }
+  def pcCloseCombatZone_(n) = {
+    _pcCloseCombatZone = n
   }
   /** Updates the field using double dispatch
    * but i ran out of time to implement this.
@@ -25,11 +43,19 @@ class Field(var _weatherZone = new ArrayBuffer[Weather](1),
     if (p.getClass = "User") {
       if (z == "SiegeZone") {
         userSiegeZone_(p.sz)
-
-      } else {//same for other zones
-
+      } else if (z == "RangeZone") {
+        userRangeZone_(p.sz)
+      } else if (z == "CloseCombatZone") {
+        userCloseCombatZone_(p.sz)
       }
-    } else {//same for pc
+    } else {
+      if (z == "SiegeZone") {
+        pcSiegeZone_(p.sz)
+      } else if (z == "RangeZone") {
+        pcRangeZone_(p.sz)
+      } else if (z == "CloseCombatZone") {
+        pcCloseCombatZone_(p.sz)
+      }
     }
   }
 
